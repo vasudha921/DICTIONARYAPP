dc3502c06f5322bf30d505fbd896f6b46c8b6f5a
vasudha921 <shwethamoorthy@gmail.com>
2020-11-22 15:42:11 +0530

    added many things

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import {Header} from 'react-native-elements';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      text: '',
+      displayText: ''
+    }
+  }
+  render(){
+    return(
+      <View>
+        <Header
+          backgroundColor = 'yellow'
+          centerComponent={{text: 'My Dictionary App',style: {color:'black', fontSize:20} }}
+        />
+        <TextInput 
+        style = {styles.inputBox}
+        onChangeText={(text)=>{
+          this.setState({text:text, });
+        }}
+        value = {this.state.text}/>
+        </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles  = StyleSheet.create({
+  inputBox:{
+    marginTop:265,
+    width:'60%',
+    alignSelf:'center',
+    height:'12%',
+    textAlign:'center',
+    borderWidth:4,
+    
+
+  }
+})
